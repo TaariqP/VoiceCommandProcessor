fe51b3422aa30d329b18e2e0d366659d11a19f04
TaariqP <tp1717@ic.ac.uk>
2020-12-28 21:24:11 +0000

    remove todos

--- routes.js
+++ routes.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const todos = require("./todos");
 
 const router = express.Router();
 
 router.get("/todos", function(req, res) {
-    res.json(todos);
+    res.json("hello");
 });
 
 module.exports = router;
