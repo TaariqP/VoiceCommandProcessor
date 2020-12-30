1c60ca1b80a7d160f87eab14ceef79261ca9f34d
TaariqP <tp1717@ic.ac.uk>
2020-12-30 17:28:47 +0000

    add route and change localhost port

--- routes.js
+++ routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+
+router.get("/", function(req, res) {
+    res.json("hello");
+});
 router.get("/todos", function(req, res) {
     res.json("hello");
 });
